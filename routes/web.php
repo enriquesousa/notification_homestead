39cea3600cfb4d36ab5b4d14fef957e73d9d1ef1
Enrique <enrique.sousa@gmail.com>
2022-04-20 14:44:02 -0700

    05. Crear controlador para Dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,39 +1,17 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\MessageController;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\DashboardController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 
+Route::middleware(['auth:sanctum', config('jetstream.auth_session'),'verified'])->get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
 
 // Rutas
 Route::get('messages/{message}', [MessageController::class, 'show'])->name('messages.show');
 Route::post('messages', [MessageController::class, 'store'])->name('messages.store');
-
-
-
-
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
